aaf5886ab7b5969f5bf8d32d588d57517b960e3c
Gabi-ziasmin <Gabi-ziasmin>
2020-09-21 10:01:27 -0300

    Exercise6

--- Election.cs
+++ Election.cs
@@ -1,19 +1,22 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using Xunit;
 
-namespace Entra21_tests
+namespace entra21_tests
 {
     public class Election
-    { 
-        //ToDo: should assert candidates list is empy
+    {
+        // Propriedade abaixo:
+        // Sempre em PascalCase
+        public List<(int id, string name, int votes)> Candidates { get; set; }
+        
         public bool CreateCandidates(List<(int id, string name)> candidates, string password)
         {
             if (password == "Pa$$w0rd")
             {
-                //Criar candidatos
-                var myCandidates = candidates;
+                Candidates = candidates.Select(candidate => {
+                    return (candidate.id, candidate.name, 0);
+                }).ToList();
+
                 return true;
             }
             else
@@ -21,10 +24,21 @@ namespace Entra21_tests
                 return false;
             }
         }
-        public bool GetCandidates(List<(int id, string name)> candidates, string password)
+
+        public List<string> ShowMenu()
         {
-            
+            return Candidates
+                .Select(candidate => $"Vote {candidate.id} para o candidato: {candidate.name}")
+                .ToList();
         }
-    } 
-    
+
+        public void Vote(int id)
+        {
+            Candidates = Candidates.Select(candidate => {
+                return candidate.id == id
+                    ? (candidate.id, candidate.name, candidate.votes + 1)
+                    : candidate;
+            }).ToList();
+        }
+    }
 }
