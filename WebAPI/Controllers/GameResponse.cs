1fd311e2996b2231c5657bdf5e9ea6223877d091
Entra21 <entra21@proway.treina>
2020-10-30 21:08:15 -0300

    WebAPI

---
+++ WebAPI/Controllers/GameResponse.cs
@@ -0,0 +1,13 @@
+namespace WebAPI.Controllers
+{
+
+    public class GameResponse
+    {
+
+        public string Name {get; set; }
+        
+        public string Plataform {get; set; }
+
+        public double Price {get; set; }
+    }
+}
---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public List<GameResponse> Get()
+        {
+            var game = new GameResponse
+            {
+                Name = "Dark Souls",
+                Plataform = "MultiPlataforma",
+                Price = 150
+            };
+
+            return new List<GameResponse>{game};
+        }
+    }
+}
