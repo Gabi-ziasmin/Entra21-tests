a24019f2a4ed4dcd88115fa3cef0600ec1296bb2
Gabi-ziasmin <gabigabrielli38@gmail.comn>
2020-12-02 20:42:25 -0300

    Migrations

--- Domain/Infra/Dbcontext.cs
+++ Domain/Infra/Dbcontext.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+using Domain.Players;
 using Domain.Users;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,6 +8,8 @@ namespace Domain.Infra
     public class Dbcontext : DbContext
     {
         public DbSet<User> Users { get; set; }
+        public DbSet<Player> Players {get; set; }
+
         // public DbSet<Player> Players { get; set; }
 
         // override, pois estamos sobrescrevando o comportamento/método padrão
@@ -15,5 +19,12 @@ namespace Domain.Infra
             // PWD = password
             optionsBuilder.UseSqlServer("Data Source=localhost;User Id=sa;PWD=some(!)Password;Initial Catalog=Brasileirao");
         } 
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(
+                Assembly.GetExecutingAssembly()
+            );
+        }
     }
 }
